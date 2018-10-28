7ee84e7d1ff8c03b884c737241a1e154dd5c3ba3
thanuwat.san <thanusanla@gmail.com>
2018-10-28 17:40:30 +0700

    Update rest repository.

--- src/main/java/th/co/abs/map/service/repository/PlaceRepository.java
+++ src/main/java/th/co/abs/map/service/repository/PlaceRepository.java
@@ -1,5 +1,8 @@
 package th.co.abs.map.service.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +14,15 @@ import th.co.abs.map.service.entity.Places;
 @RepositoryRestResource(collectionResourceRel = "Places", path = "Places")
 public interface PlaceRepository extends CrudRepository<Places, Long> {
 	
-	//Places findById(@Param("id") String id);
+	@Query("SELECT p FROM Places p " +
+            "WHERE 1=1 " +
+            "ORDER BY p.objectid ")
+    List<Places> getAllPlaces();
+	
+	@Query("SELECT p FROM Places p " +
+            "WHERE 1=1 " +
+            "ORDER BY p.objectid ")
+    Optional<List<Places>> getAllPlacesByOptional();
 
 	Places findByObjectid(@Param("object_id") Long object_id);
 }
